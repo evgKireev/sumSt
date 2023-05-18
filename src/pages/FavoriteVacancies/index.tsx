4bb1c5446dc0cef0458951084426da3d892b8600
evgKireev <bob1_k@mail.ru>
2023-05-19 00:28:17 +0300

    add pogination

--- src/pages/FavoriteVacancies/index.tsx
+++ src/pages/FavoriteVacancies/index.tsx
@@ -5,31 +5,44 @@ import Card from '../../components/Card'
 import styles from './FavoriteVacancies.module.scss'
 import NoCard from '../../components/NoCard'
 import { useEffect } from 'react'
+import usePagination from '../../hooks/usePagination'
+import { Pagination } from '@mantine/core'
 
 const FavoriteVacancies = () => {
   const { vacanciesFavorites } = useAppSelector((state) => state.vacanciesSlice)
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(vacanciesFavorites))
   }, [vacanciesFavorites])
+  const { firstContentIndex, lastContentIndex, page, setPage, totalPages } = usePagination({
+    contentPerPage: 4,
+    count: vacanciesFavorites.length
+  })
   return (
-    <div className={styles.wrap}>
-      {!vacanciesFavorites.length ? (
-        <NoCard text={'Упс, здесь еще ничего нет!'} />
-      ) : (
-        vacanciesFavorites.map((value) => (
-          <Card
-            key={value.id}
-            id={value.id}
-            profession={value.profession}
-            town={value.town.title}
-            typeWork={value.type_of_work.title}
-            paymentTo={value.payment_to}
-            paymentFrom={value.payment_from}
-            currency={value.currency}
-          />
-        ))
+    <>
+      <div className={styles.wrap}>
+        {!vacanciesFavorites.length ? (
+          <NoCard text={'Упс, здесь еще ничего нет!'} />
+        ) : (
+          <div className="items">
+            {vacanciesFavorites.slice(firstContentIndex, lastContentIndex).map((value: any) => (
+              <Card
+                key={value.id}
+                id={value.id}
+                profession={value.profession}
+                town={value.town.title}
+                typeWork={value.type_of_work.title}
+                paymentTo={value.payment_to}
+                paymentFrom={value.payment_from}
+                currency={value.currency}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      {!vacanciesFavorites.length ? null : (
+        <Pagination className={styles.pagination} value={page} onChange={setPage} total={totalPages} />
       )}
-    </div>
+    </>
   )
 }
 
